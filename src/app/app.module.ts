3bacebbd33ee3057d3b96edd999c7edb624d0276
Daniel Simmonds <simmondsdt@gmail.com>
2017-05-26 10:17:31 -0600

    organize exports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,18 @@ import { appRoutes } from './routes';
 import { NavBarComponent } from './nav/navbar.component';
 import { Error404Component } from './errors/404.component';
 
-import { EventListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
+
+import {
+	EventListComponent,
+	EventThumbnailComponent,
+	EventDetailsComponent,
+	CreateEventComponent,
+	EventService,
+	EventRouteActivator,
+	EventListResolver,
+} from './events/index'
+
 
 @NgModule({
 	declarations: [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Error404Component } from './errors/404.component';
-import { EventListComponent } from './events/events-list.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
+import {
+	EventListComponent,
+	EventDetailsComponent,
+	CreateEventComponent,
+	EventRouteActivator,
+	EventListResolver,
+} from './events/index'
 
 export const appRoutes:Routes = [
 	{ path: 'events/new',
@@ -19,4 +21,5 @@ export const appRoutes:Routes = [
 
 	{ path: '404', component: Error404Component },
 	{ path: '', redirectTo: '/events', pathMatch: 'full' },
+	{ path: 'user', loadChildren: 'app/user/user.module#UserModule', }
 ]
