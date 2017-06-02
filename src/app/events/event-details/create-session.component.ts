5e82acd45d5e9b30134249350bf7019363e65e50
Daniel Simmonds <simmondsdt@gmail.com>
2017-06-02 10:51:53 -0600

    add custom validator to session form

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ISession } from '../shared/index';
+import { ISession, restrictedWords } from '../shared/index';
 
 @Component({
 	templateUrl: './create-session.component.html',
@@ -20,7 +20,7 @@ export class CreateSessionComponent implements OnInit {
 		this.presenter = new FormControl('', Validators.required)
 		this.duration = new FormControl('', Validators.required)
 		this.level = new FormControl('', Validators.required)
-		this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+		this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400),restrictedWords(['foo', 'bar']) ])
 
 		this.newSessionForm = new FormGroup({
 			name: this.name,
@@ -30,7 +30,7 @@ export class CreateSessionComponent implements OnInit {
 			abstract: this.abstract,
 		})
 	}
-
+	
 	saveSession(formValues) {
 		let session:ISession = {
 			id: undefined,
