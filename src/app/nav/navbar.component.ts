5d5e72f00dc47016378dbccfc3e6ae982e8cb181
Daniel Simmonds <simmondsdt@gmail.com>
2017-06-07 12:29:06 -0600

    add ability to search

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { ISession } from '../events/shared/event.model';
+import { EventService } from '../events/index';
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './navbar.component.html',
-  styleUrls: [ '../styles/navbar.scss' ],
+	selector: 'nav-bar',
+	templateUrl: './navbar.component.html',
+	styleUrls: [ '../styles/navbar.scss' ],
 })
 
 export class NavBarComponent {
-  constructor(private auth:AuthService) {}
+	searchTerm: string = "";
+	foundSessions: ISession[];
+
+	constructor(
+		private auth:AuthService,
+		private eventService:EventService,
+	) {}
+
+	searchSessions(searchTerm) {
+		this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+			this.foundSessions = sessions;
+			console.log(this.foundSessions);
+		})
+	}
 }
